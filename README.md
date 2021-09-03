dc3b4c3f5887ca832c750cd4ac521ded6aaca859
Apdo3939 <apdo3939@hotmail.com>
2021-09-03 16:38:17 -0300

    Add routes and pages

---
+++ README.md
@@ -0,0 +1,21 @@
+Run the project use expo:
+
+### yarn start
+
+### expo start
+
+To navigate between pages:
+
+### yarn add @react-navigation/native
+
+### expo install react-native-screens react-native-safe-area-context
+
+### yarn add @react-navigation/stack
+
+### expo install react-native-gesture-handler
+
+### expo install @react-native-masked-view/masked-view
+
+Call integration with API
+
+### yarn add axios
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    const screenOptions = {
+        headerStyle: {
+            backgroundColor: '#223322'
+        },
+        headerTintColor: '#bbbbbb'
+    }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={screenOptions}>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerTitle: 'Listar Orçamento' }} />
+                <Stack.Screen name="Contact" component={Contact} />
+                <Stack.Screen name="Services" component={Services} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
